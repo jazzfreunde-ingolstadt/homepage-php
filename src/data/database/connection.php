39d78190b84457244576f436458b64218e2256e2
Michael Mayer <miche.mayer@outlook.de>
2021-05-14 00:16:45 +0000

    setup local database connection with credentials through env vars.

--- src/data/database/connection.php
+++ src/data/database/connection.php
@@ -4,24 +4,14 @@ namespace Jazzfreunde\Database;
 
 use PDO;
 use PDOException;
+use PDOStatement;
 
 final class Connection
 {
-    private PDO $conn;
+    private ?PDO $conn = null;
 
     public function __construct(private Credentials $credentials)
     {
-    }
-
-    public function GetConnection(): PDO
-    {
-        try {
-            if (!$this->conn)
-                $this->connect($this->credentials);
-        } catch (\PDOException $e) {
-            throw new ConnectionException($e->getMessage());
-        }
-        return $this->conn;
     }
 
     private function connect(Credentials $credentials): void
@@ -34,6 +24,39 @@ final class Connection
         $this->conn->exec("set names utf8");
         $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function GetConnection(): PDO
+    {
+        try {
+            if (!$this->conn)
+                $this->connect($this->credentials);
+        } catch (\PDOException $e) {
+            throw new ConnectionException($e->getMessage(), 0, $e);
+        }
+        return $this->conn;
+    }
+
+    public function fetch(string $class, string $query, ?array $parameters = null): array
+    {
+        try {
+            $stmt = $this->GetConnection()->prepare($query);
+            $stmt->execute($parameters);
+        } catch (\PDOException $e) {
+            throw new ConnectionException("Execution did not succeed. ${query}", 0, $e);
+        }
+
+        return $stmt->fetchall(\PDO::FETCH_CLASS, $class);
+    }
+
+    public function execute(string $query, ?array $parameters = null): void
+    {
+        try {
+            $stmt = $this->GetConnection()->prepare($query);
+            $stmt->execute($parameters);
+        } catch (\PDOException $e) {
+            throw new ConnectionException("Execution did not succeed. ${query}", 0, $e);
+        }
     }
 }
 
