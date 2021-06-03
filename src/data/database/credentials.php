2adb11ef8891c601bc4d1bd1afc3aa0488380452
Michael Mayer <miche.mayer@outlook.de>
2021-06-03 14:32:21 +0000

    make sure to hide credentials from debug

--- src/data/database/credentials.php
+++ src/data/database/credentials.php
@@ -5,18 +5,19 @@ namespace Jazzfreunde\Database;
 class Credentials
 {
     public function __construct(
-        public string $host, 
-        public string $database, 
+        public string $host,
+        public string $database,
         public string $user,
         public string $password
-    ) {}
+    ) {
+    }
 
     public static function LoadFromEnv(
         string $host_env,
         string $database_env,
         string $user_env,
         string $password_env
-        ): Credentials {
+    ): Credentials {
 
         return new Credentials(
             @$_ENV[$host_env] ?? '',
@@ -24,5 +25,20 @@ class Credentials
             @$_ENV[$user_env] ?? '',
             @$_ENV[$password_env] ?? ''
         );
+    }
+
+    public function __debugInfo()
+    {
+        return [
+            'host' => '********',
+            'database' => $this->database,
+            'user' => '********',
+            'password' => '********'
+        ];
+    }
+
+    public function __toString()
+    {
+        return $this->database;
     }
 }
