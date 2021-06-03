c17f2c7b3de9af433af75af2490f462fa5736089
Michael Mayer <miche.mayer@outlook.de>
2021-06-03 22:33:44 +0000

    added new main page handler.

--- src/public/index.php
+++ src/public/index.php
@@ -1,11 +1,8 @@
 <?php
 
-$ns_mapping = function () {
-    yield 'jazzfreunde/database' => 'data/database';
-    yield 'jazzfreunde/storage' => 'data/storage';
-    yield 'jazzfreunde/environment' => 'environment';
-    yield 'jazzfreunde/structures' => 'structures';
-    yield 'jazzfreunde/app' => 'app';
-};
-
 require(__DIR__ . '/../app/bootstrap/autoload.php');
+
+if (file_exists(__DIR__ . "/pages{$_SERVER['REQUEST_URI']}.php"))
+    include(__DIR__ . "/pages{$_SERVER['REQUEST_URI']}.php");
+else
+    include('pages/404.php');
