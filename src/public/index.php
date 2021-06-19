58ed7a5ffb06b2850bcfbcb0d091a1a9f4acc90f
Michael Mayer <miche.mayer@outlook.de>
2021-06-19 13:38:04 +0000

    debug document root in stage environment.

--- src/public/index.php
+++ src/public/index.php
@@ -1,10 +1,11 @@
 <?php
 
-$app = require __DIR__ . '/../app/startup.php';
-
-if (!preg_match('/^\/(.+?)(?:\/)?$/', $_SERVER['REQUEST_URI'], $route))
-    include __DIR__ . "/legacy/pages/index.php";
-elseif (file_exists(__DIR__ . "/pages/{$route[1]}.php"))
-    include __DIR__ . "/pages/{$route[1]}.php";
-else
+$app = require __DIR__.'/../app/startup.php';
+var_dump($_SERVER['DOCUMENT_ROOT']);
+if (!preg_match('/^\/(.+?)(?:\/)?$/', $_SERVER['REQUEST_URI'], $route)) {
+    include __DIR__."/legacy/pages/index.php";
+} elseif (file_exists(__DIR__."/pages/{$route[1]}.php")) {
+    include __DIR__."/pages/{$route[1]}.php";
+} else {
     include 'pages/404.php';
+}
