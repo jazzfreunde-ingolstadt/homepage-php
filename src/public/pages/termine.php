cd9494a482e43f70a2badbcfc633911954aab212
Michael Mayer <miche.mayer@outlook.de>
2021-06-24 16:57:11 +0000

    comform to symfony standard

--- src/public/pages/termine.php
+++ src/public/pages/termine.php
@@ -2,8 +2,8 @@
 define("PAGE", "termine");
 define("TITLE", "Veranstaltungen");
 
-include_once __DIR__ . "/../legacy/inc/environment.php";
-$termine_list = $app->Include('termine_list');
+include_once __DIR__."/../legacy/inc/environment.php";
+$termineList = $app->Include('termine_list');
 
 head();
 before();
@@ -12,7 +12,6 @@ before();
 <h1>Veranstaltungskalenderer</h1>
 <?php
 
-$termine_list();
+$termineList();
 
 after();
-?>
