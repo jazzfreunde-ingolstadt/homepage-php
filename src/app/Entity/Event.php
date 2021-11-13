5b3c40c9edcf69d44f7553fd710805c892d9ccaf
Michael Mayer <miche.mayer@outlook.de>
2021-11-13 20:02:49 +0000

    constructor property promotion

--- src/app/Entity/Event.php
+++ src/app/Entity/Event.php
@@ -4,48 +4,44 @@ namespace Jazzfreunde\App\Entity;
 
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
-use Jazzfreunde\App\Structures\DateTimeSQL;
 
-// #[ORM\Entity(repositoryClass: EventRepository::class)]
-#[ORM\Entity()]
+/**
+ * Terminierte Veranstaltung
+ */
+#[ORM\Entity(repositoryClass: \Jazzfreunde\App\Model\EventRepository::class)]
 #[ORM\Table(name: 'events')]
 final class Event
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: 'integer')]
-    private ?int $id;
-    // private ?int $series_id,
-    #[ORM\Column(type: 'string')]
-    private string $titel;
-    #[ORM\Column(type: 'string')]
-    private ?string $subtitel;
-    #[ORM\Column(type: 'datetime')]
-    private DateTime $start;
-    #[ORM\Column(type: 'datetime')]
-    private DateTime $end;
-    #[ORM\Column(type: 'string')]
-    private string $ort;
-    #[ORM\Column(type: 'string', nullable: true)]
-    private ?string $link;
-    // private ?string $thumbnail
-
+    /**
+     * Immutable Object
+     *
+     * @param string|null $id
+     * @param string      $titel
+     * @param string      $subtitel
+     * @param DateTime    $start
+     * @param DateTime    $end
+     * @param string      $ort
+     * @param string|null $link
+     */
     public function __construct(
-        ?string $id = null,
-        string $titel,
-        string $subtitel,
-        DateTime $start,
-        DateTime $end,
-        string $ort,
-        ?string $link
+        #[ORM\Id]
+        #[ORM\GeneratedValue]
+        #[ORM\Column(type: 'integer')]
+        private ?int $id,
+        // private ?int $series_id,
+        #[ORM\Column(type: 'string')]
+        private string $titel,
+        #[ORM\Column(type: 'string')]
+        private ?string $subtitel,
+        #[ORM\Column(type: 'datetime')]
+        private DateTime $start,
+        #[ORM\Column(type: 'datetime')]
+        private DateTime $end,
+        #[ORM\Column(type: 'string')]
+        private string $ort,
+        #[ORM\Column(type: 'string', nullable: true)]
+        private ?string $link
     ) {
-        $this->id = $id;
-        $this->titel = $titel;
-        $this->subtitel = $subtitel;
-        $this->start = $start;
-        $this->end = $end;
-        $this->ort = $ort;
-        $this->link = $link;
     }
 
     /**
