d0dff867434d0d149e276070f940d08ae9758686
Michael Mayer <miche.mayer@outlook.de>
2021-11-14 21:59:33 +0000

    - Kommtatre & Link angepasst

--- src/legacy/pages/ueberuns.php
+++ src/legacy/pages/ueberuns.php
@@ -3,7 +3,7 @@ if (defined("PAGE")) die("Wrong reference.");
 define("PAGE", "ueberuns");
 define("TITLE", "Über uns");
 
-include_once __DIR__ . "/../inc/environment.php";
+include_once dirname(__DIR__)."/inc/environment.php";
 
 head();
 before();
@@ -57,47 +57,6 @@ before();
     <div class="left"><a class="kontakt" href="/kontakt/?to=bachmaier" title="Zum Kontaktformular">Kontakt</a></div>
   </div>
 </div>
-<!--
-<div class="kasten person odd"><img src="/gfx/gutsche_pre.png" alt="Bild" height="90" width="70" />
-<div class="kasten_h1">Schatzmeister</div>
-<div class="kasten_h2">Matthias Gutsche</div>
-<div class="kasten_p">
-<div class="right"><a class="kontakt" href="/kontakt/?to=gutsche" title="Zum Kontaktformular">Kontakt</a></div></div>
-</div>
--->
-<!--
-<div class="kasten person odd"><img src="/gfx/spranger_pre.png" alt="Bild" height="90" width="70" />
-<div class="kasten_h1">Schatzmeister</div>
-<div class="kasten_h2">Ulrich Spranger</div>
-<div class="kasten_p">Jahrgang 1960 und als echter Schanzer in Ingolstadt geboren und aufgewachsen. Nach dem Studium in Bayreuth, ist er  selbständig als Steuerberater und Wirtschaftsprüfer in Ingolstadt tätig. Selbst begeisterter Jazzzuhörer, bei dem es leider zum Selberspielen nicht mehr gereicht hat. Lieblingsinstrumente sind Sax, Gitarre, Klavier und Akkustikbass. Hört alles gern was jazzig, bluesig und funkig klingt.
-<div class="right"><a class="kontakt" href="/kontakt/?to=spranger" title="Zum Kontaktformular">Kontakt</a></div></div>
-</div>
--->
-<!--
-<div class="kasten person even"><img src="/gfx/bleckmann_pre.png" alt="Bild" height="90" width="70" />
-<div class="kasten_h1">Beirat</div>
-<div class="kasten_h2">Sven Wittig</div>
-<div class="kasten_p">Sven wuchs in Chemnitz und Dresden auf und erlernte im Elternhaus das Klavierspielen. Später kam das Saxophon auf autodidaktischem Wege hinzu. Erste Eindrücke aus der Dresdner Dixieland-Szene wurden bald durch die Anziehungskraft des Leipziger, Berliner und auch Chemnitzer Modern- und Avantgarde-Jazz-Umfelds übertönt. Die Begeisterung für die unglaubliche Vielfalt und Kreativität der unterschiedlichen Strömungen des Jazz wuchs stetig.<br />
-Sven arbeitet als Informatiker bei der AUDI AG. In seiner Freizeit wirkt er gern in verschiedenen Bands und vor allem den Sessions der „Jazzfreunde“ mit.
-<div class="right"><a class="kontakt" href="/kontakt/?to=bleckmann" title="Zum Kontaktformular">Kontakt</a></div></div>
-</div>
-<div class="kasten person odd"><img src="/gfx/domke_pre.png" alt="Bild" height="90" width="70" />
-<div class="kasten_h1">Beirat</div>
-<div class="kasten_h2">Reimund Domke</div>
-<div class="kasten_p">Geboren 1956, aufgewachsen in Kronach, Studium Touristik BWL, 10 Jahre Frankfurt<br />
-Seit 1993 in Ingolstadt – Filialleitung DER Reisebüro<br />
-Erste Jazzbegegnung im Alter von 17 Jahren gleich mit dem „Höhepunkt“ Keith Jarrett in der Aula des Gymnasiums Kronach (wäre heute nicht mehr vorstellbar). Danach folgten  Jan Garbarek, Billy Cobham und in Rom auf Klassenfahrt Dizzy Gillespie in den Caracalla Thermen – ein weiteres Highlight in jungen Jahren.<br />
-Wiedereinstieg in den Jazz 1995 in Ingolstadt zu den Jazztagen, seit 1999 Mitarbeit bei den Jazztagen in der Sponsoren- und Gästebetreuung, Gründungsmitglied der Jazzfreunde Ingolstadt im Jahr 2005 und bis 2014 1.&nbsp;Vorsitzender.
-<div class="right"><a class="kontakt" href="/kontakt/?to=domke" title="Zum Kontaktformular">Kontakt</a></div></div>
-</div>
--->
-<!--
-<div class="kasten person even"><img src="/gfx/hoepp_pre.png" alt="Bild" height="90" width="70" />
-<div class="kasten_h1">Webmaster</div>
-<div class="kasten_h2">Daniel J. Höpp</div>
-<div class="kasten_p">Daniel Höpp ist seit 2010 Testingenieur in einem Unternehmen in der Automobilbranche, zuvor hat er Elektro- und Informationstechnik an der Fachhochschule Ingolstadt studiert. Er betreut, gestaltet und programmiert bereits seit mehreren Jahren erfolgreich die Homepages von verschiedenen Organisationen, neben den Jazzfreunden unter anderem auch die Seiten der Landesarbeitsgemeinschaft Schulchor in Bayern.
-<div class="right"><a class="kontakt" href="/kontakt/?to=hoepp" title="Zum Kontaktformular">Kontakt</a></div></div>
-</div>-->
 <div class="kasten person even"><img src="/gfx/mayer_pre.jpg" alt="Bild" height="90" width="70" />
   <div class="kasten_h1">Webmaster</div>
   <div class="kasten_h2">Michael Mayer</div>
@@ -109,38 +68,6 @@ Wiedereinstieg in den Jazz 1995 in Ingolstadt zu den Jazztagen, seit 1999 Mitarb
     <div class="right"><a class="kontakt" href="/kontakt/?to=mayer" title="Zum Kontaktformular">Kontakt</a></div>
   </div>
 </div>
-<!--
-<div class="kasten person odd"><img src="/gfx/mayer_pre.png" alt="Bild" height="90" width="70" />
-<div class="kasten_h1">Beirat</div>
-<div class="kasten_h2">Michael Mayer</div>
-<div class="kasten_p">&nbsp;
-<div class="right"><a class="kontakt" href="/kontakt/?to=mayer" title="Zum Kontaktformular">Kontakt</a></div></div>
-</div>
-<div class="kasten person even"><img src="/gfx/mittnacht_pre.png" alt="Bild" height="90" width="70" />
-<div class="kasten_h1">Beirätin</div>
-<div class="kasten_h2">Ella Mittnacht</div>
-<div class="kasten_p">&nbsp;
-<div class="right"><a class="kontakt" href="/kontakt/?to=mittnacht" title="Zum Kontaktformular">Kontakt</a></div></div>
-</div>
-<div class="kasten person odd"><img src="/gfx/pacher_pre.png" alt="Bild" height="90" width="70" />
-<div class="kasten_h1">Beirat</div>
-<div class="kasten_h2">Dr.&nbsp;Christian Pacher</div>
-<div class="kasten_p">Selbstständiger Apotheker in Ingolstadt. Kam durch Konzertbesuche von Miles Davis während des Studiums in München zum Jazz. Dr.&nbsp;Christian Pacher kriecht seit 1983 vor allen Konzertbühnen in und um Ingolstadt mit der Kamera herum und fotografiert speziell Blues- und Jazzkonzerte. Die Ergebnisse waren in zahlreichen Ausstellungen sichtbar. Mit der Gruppe Forum Fotografie veröffentlichte er zu den 20.&nbsp;Ingolstädter Jazztagen den Fotobildband <i>jazz in time</i>.
-<div class="right"><a class="kontakt" href="/kontakt/?to=pacher" title="Zum Kontaktformular">Kontakt</a></div></div>
-</div>
-<div class="kasten person even"><img src="/gfx/righetti_pre.png" alt="Bild" height="90" width="70" />
-<div class="kasten_h1">Beirat</div>
-<div class="kasten_h2">Raphael Righetti</div>
-<div class="kasten_p">&nbsp;
-<div class="right"><a class="kontakt" href="/kontakt/?to=righetti" title="Zum Kontaktformular">Kontakt</a></div></div>
-</div>
-<div class="kasten person odd"><img src="/gfx/spranger-righetti_pre.png" alt="Bild" height="90" width="70" />
-<div class="kasten_h1">Beirätin</div>
-<div class="kasten_h2">Eva Spranger-Righetti</div>
-<div class="kasten_p">Schanzerin und Alt-Gnadenthalerin, daher schon früh mit Geige und Klavier etc. konfrontiert, heute Klavier und Orgel u. a., soweit die Zeit reicht. Begeisterte Jazz-Hörerin, tätig in einer PR-Agentur.
-<div class="right"><a class="kontakt" href="/kontakt/?to=spranger-righetti" title="Zum Kontaktformular">Kontakt</a></div></div>
-</div>
--->
 
 <h2>Impressum</h2>
 <div id="impressum">
@@ -162,20 +89,11 @@ Wiedereinstieg in den Jazz 1995 in Ingolstadt zu den Jazztagen, seit 1999 Mitarb
     Telefon: 08458 5597<br />
     <a class="kontakt" href="/kontakt/?to=mayer" title="Zum Kontaktformular">Kontakt</a>
   </p>
-
-  <!--
-<p><strong>Webmaster:</strong><br />
-Daniel J. Höpp<br />
-Weigelstraße 4<br />
-85055 Ingolstadt<br />
-Telefon: 0841 / 9511539<br />
-<a class="kontakt" href="/kontakt/?to=hoepp" title="Zum Kontaktformular">Kontakt</a></p>
--->
 </div>
 
 <p class="small">Es is untersagt, die innerhalb der Internetpräsenz der Jazzfreunde Ingolstadt angegebenen Kontaktdaten für Werbung oder unerwünschte Kontaktierung (i.&nbsp;d.&nbsp;R. nicht den Verein betreffende Angelegenheiten, Umfragen, Statistiken etc.) zu speichern oder zu verwenden!</p>
 
-<p>Wenn Sie interessiert, was wir mit Ihren Daten anfangen und wie wir sie schützen, lesen Sie unsere Informationen zu <a href="daten.php">Datenerhebung und Datenschutz</a>.</p>
+<p>Wenn Sie interessiert, was wir mit Ihren Daten anfangen und wie wir sie schützen, lesen Sie unsere Informationen zu <a href="/daten/">Datenerhebung und Datenschutz</a>.</p>
 
 <?
 after();
