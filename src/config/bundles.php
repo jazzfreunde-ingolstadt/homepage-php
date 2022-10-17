7d5561c0acce89dc4e21664fc5fb6fa43d55bbc8
Miche Mayer <miche.mayer@outlook.de>
2022-10-17 18:22:26 +0000

    + api platform

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -1,10 +1,12 @@
 <?php
 
+use ApiPlatform\Symfony\Bundle\ApiPlatformBundle;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\MakerBundle\MakerBundle;
 use Symfony\Bundle\MonologBundle\MonologBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
 
 return [
     FrameworkBundle::class => ['all' => true],
@@ -12,4 +14,6 @@ return [
     DoctrineMigrationsBundle::class => ['all' => true],
     MakerBundle::class => ['dev' => true],
     MonologBundle::class => ['all' => true],
+    ApiPlatformBundle::class => ['all' => true],
+    TwigBundle::class => ['all' => true],
 ];
