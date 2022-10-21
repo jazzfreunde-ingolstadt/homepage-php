d442ac05ee76af8e640c13d808ac36306cb451ed
Miche Mayer <miche.mayer@outlook.de>
2022-10-21 16:46:00 +0000

    Verwende absolute Pfade

--- src/app/Kernel.php
+++ src/app/Kernel.php
@@ -27,12 +27,12 @@ class Kernel extends BaseKernel
      */
     protected function configureContainer(ContainerConfigurator $container): void
     {
-        $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
+        $container->import(\dirname(__DIR__).'/config/{packages}/*.yaml');
+        $container->import(\dirname(__DIR__).'/config/{packages}/'.$this->environment.'/*.yaml');
 
         if (is_file(\dirname(__DIR__).'/config/services.yaml')) {
-            $container->import('../config/services.yaml');
-            $container->import('../config/{services}_'.$this->environment.'.yaml');
+            $container->import(\dirname(__DIR__).'/config/services.yaml');
+            $container->import(\dirname(__DIR__).'/config/{services}_'.$this->environment.'.yaml');
         }
     }
 
@@ -41,11 +41,11 @@ class Kernel extends BaseKernel
      */
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
-        $routes->import('../config/{routes}/*.yaml');
+        $routes->import(\dirname(__DIR__).'/config/{routes}/'.$this->environment.'/*.yaml');
+        $routes->import(\dirname(__DIR__).'/config/{routes}/*.yaml');
 
         if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
-            $routes->import('../config/routes.yaml');
+            $routes->import(\dirname(__DIR__).'/config/routes.yaml');
         }
     }
 }
