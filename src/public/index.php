914c2611ca1435eb52f7ec515d0b711cfd4cf327
Miche Mayer <miche.mayer@outlook.de>
2022-12-26 19:24:25 +0000

    Integriere Wartungsmodus in Deploy Workflow

--- src/public/index.php
+++ src/public/index.php
@@ -2,6 +2,11 @@
 
 use Jazzfreunde\App\Kernel;
 
+if (\file_exists(__DIR__.'/maintenance.htm')) {
+    http_response_code(503);
+    exit('Diese Seite befindet sich aktuell in Wartung. Bitte versuchen Sie es zu einem späteren Zeitpunkt erneut.');
+}
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
