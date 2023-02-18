9cc70e04becff47d32e7c3b2cd344276112c30e6
Miche Mayer <miche.mayer@outlook.de>
2023-02-18 23:04:43 +0000

    Entferne noch mehr alte Assets

--- src/app/Controller/AppRoutingController.php
+++ src/app/Controller/AppRoutingController.php
@@ -59,21 +59,21 @@ class AppRoutingController extends AbstractController
                 'bio' => 'Karl Wewer ist aufgewachsen in Datteln (Ruhrgebiet); Studium konstruktiver Ingenieurbau in Bochum, seit 1979 in Ingolstadt – Manager in der IT in einem Automobilunternehmen, jetzt im Ruhestand<br />
                 Er hat in jungen Jahren in mehren Rock- und Beat Bands im Ruhrgebiet Gitarre gespielt. Seit 1991 ist er Gitarrist in der Ingolstädter „<a href="http =>//www.kraiberg-jazz-band.de">Kraiberg Jazz Band</a>“. Daneben ist er begeisterter Hörer von Livekonzerten mit dem Schwerpunkt Jazz / Blues / Rock.<br />
                 Seit 2010 ist er Mitglied im Verein und wurde 2011 zum Schriftführer und 2014 zum ersten Vorsitzenden gewählt; er versendet zudem den Jazz–Newsletter des Vereins.',
-                'pictureSrc' => '/gfx/wewer_pre.png',
+                'pictureSrc' => 'build/app/images/people/wewer.png',
                 'mail' => 'wewer',
             ],
             [
                 'name' => 'Tom Diewock',
                 'position' => 'Stellvertretender Vorsitzender, Sessionkoordinator',
                 'bio' => 'Tom Diewock ist Musiker und stellvertretender Leiter der Musikschule Mainburg. Er spielt neben seiner umfangreichen Unterrichtstätigkeit in einer Vielzahl von Bands verschiedener Stilrichtungen, u.&nbsp;a. Wes Mackey, Keith Thopmson, The Blues Hunt, Fiona Boyes, Jamazzing, 4 Of A Kind, Oliver Wasilesku Trio, Captain’s Bog, The Bomb, Clausius Konrad, Charly Böck Latin Project, Late Night Band der Ingolstädter Jazztage...',
-                'pictureSrc' => '/gfx/diewock_pre.png',
+                'pictureSrc' => 'build/app/images/people/diewock.png',
                 'mail' => 'diewock',
             ],
             [
                 'name' => 'Helmut Bachmaier',
                 'position' => 'Schatzmeister',
                 'bio' => 'Helmut Bachmaier lebt in Ingolstadt und arbeitete in München bei einer internationalen Bank. Er ist seit 2007 Mitglied bei den Jazzfreunden und seit 2020 Schatzmeister. Theater, Kabarett und Livemusik sind in der Freizeit fester Bestandteil.',
-                'pictureSrc' => '/gfx/bachmaier.jpeg',
+                'pictureSrc' => 'build/app/images/people/bachmaier.jpeg',
                 'mail' => 'bachmaier',
             ],
             [
@@ -84,7 +84,7 @@ class AppRoutingController extends AbstractController
                 Später kaufte er sich kurzer Hand einen gebrauchten Kontrabass und fing an, darauf umzusetzen, was er sich am E-Bass beigebracht hatte.
                 Zu seinen musikalischen Idolen zählen Chic Korea, Frank Zappa, Allan Holdsworth, Jacob Collier, Robert Glasper, Derek Trucks, Joni Mitchell, um nur ein paar wenige zu nennen.
                 Privat ist der gelernte Softwareentwickler gerne auf dem Rennrad unterwegs. Dann aber notgedrungen ohne Kontrabass.',
-                'pictureSrc' => '/gfx/mayer_pre.jpg',
+                'pictureSrc' => 'build/app/images/people/mayer.jpg',
                 'mail' => 'mayer',
             ]
         ];
