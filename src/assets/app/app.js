b85266e4dd5243fe5517ab0a19f40ec1992e45a6
Miche Mayer <miche.mayer@outlook.de>
2023-02-18 13:09:25 +0000

    #13

--- src/assets/app/app.js
+++ src/assets/app/app.js
@@ -5,4 +5,31 @@ global.documentReady = $.Deferred();
 
 $(() => {
   documentReady.resolve();
+
+  const body = $("body");
+  const navigation = $("#main-navigation");
+  const navclose = $(".main-navigation-close");
+  const navLogo = $(".nav-logo");
+
+  const menuExpanded = () => "true" == navigation.attr("aria-expanded");
+  const isFullScreenMenu = () => "fixed" === navigation.css("position");
+  const setExpanded = (expanded) =>
+    navigation.attr("aria-expanded", expanded ? "true" : "false");
+
+  const openMenu = () => {
+    isFullScreenMenu() && body.addClass("overflow-y-hidden");
+    setExpanded(true);
+  };
+
+  const closeMenu = () => {
+    isFullScreenMenu() && body.removeClass("overflow-y-hidden");
+    setExpanded(false);
+  };
+
+  const toggleMenu = () => {
+    menuExpanded() ? closeMenu() : openMenu();
+  };
+
+  navLogo.on("click", toggleMenu);
+  navclose.on("click", closeMenu);
 });
